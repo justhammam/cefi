e10c98cf66b02646afb611447201bb41970c103d
justhammam <just.hammam@gmail.com>
2016-09-09 02:29:22 +0100

    merubah logic

--- app/pages/add/add.ts
+++ app/pages/add/add.ts
@@ -10,19 +10,12 @@ export class AddPage {
     //sta, lebar, tebal, panjang, av lebar, av tebal, volume, staBefore, lebarBefore, tebalBefore, tebalItem
     public jalanItem: [Array<number>, Array<number>];
     public sta: number;
-    private staBefore: number;
     public lebar: number;
-    private lebarBefore: number;
 
     private tebal: number;
-    private tebalBefore: number;
     public tebalItem: Array<number>;
     public tItem: number;
     
-    private panjang: number;
-    private avLebar: number;
-    private avTebal: number; 
-    public volume: number;
     
     constructor(private nav: NavController){
         this.cekJalan = JSON.parse(localStorage.getItem("cekjalan"));
@@ -30,7 +23,7 @@ export class AddPage {
         if(!this.cekJalan){
             this.cekJalan = [];
         }
-        this.jalanItem = [[],[]];
+        this.jalanItem = [[],[],[]];
         this.tebalItem = [];
     }
 
@@ -57,29 +50,7 @@ export class AddPage {
     save(){
         this.addTebal();
         this.tebal = this.average(this.tebalItem);
-        let n = this.cekJalan.length;
-        if(n < 1){
-            this.panjang = 0;
-            this.avLebar = 0;
-            this.avTebal = 0;
-            this.volume = 0;
-            this.staBefore = 0;
-            this.lebarBefore = 0;
-            this.tebalBefore = 0;
-        }
-        if(n >= 1){
-            this.staBefore = this.cekJalan[n-1][0][0];
-            this.lebarBefore = this.cekJalan[n-1][0][1];
-            this.tebalBefore = this.cekJalan[n-1][0][2];
-
-            this.panjang = this.sta - this.staBefore;
-            this.avLebar = ((1*this.lebar) + (1*this.lebarBefore))/2;
-            this.avTebal = ((1*this.tebal) + (1*this.tebalBefore))/2;
-            this.volume = this.panjang*this.avLebar*(this.avTebal/100);
-        }
-        this.jalanItem = [[this.sta, this.lebar, this.tebal, 
-          this.panjang, this.avLebar, this.avTebal, this.volume,
-          this.staBefore, this.lebarBefore, this.tebalBefore],this.tebalItem];
+        this.jalanItem = [[this.sta, this.lebar, this.tebal],this.tebalItem];
         if(this.lebar != 0 && this.tebal != 0){
             this.cekJalan.push(this.jalanItem);
             localStorage.setItem("cekjalan", JSON.stringify(this.cekJalan));
